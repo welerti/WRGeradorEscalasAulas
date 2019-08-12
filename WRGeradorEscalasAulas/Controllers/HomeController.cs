6e937ca1139c75f5069488cafad39e0bbf78521d
welerti <welerti@gmail.com>
2019-08-12 16:57:33 -0300

    teste commit

--- WRGeradorEscalasAulas/Controllers/HomeController.cs
+++ WRGeradorEscalasAulas/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WRGeradorEscalasAulas.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "teste Contato git hub.";
 
             return View();
         }
