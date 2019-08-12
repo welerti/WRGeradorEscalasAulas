9e5e4d2dfc6114750bc3b6c941a59a07f5f3a8a2
welerti <welerti@gmail.com>
2019-08-12 16:50:11 -0300

    Add project files.

---
+++ WRGeradorEscalasAulas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WRGeradorEscalasAulas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
